015e9d715b10532cd7b31c44662ede264f415ac5
Redjorm <redjorm12@gmail.com>
2023-05-08 15:47:14 -0400

    V2

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,18 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./styles/header.css";
 const Header = () => {
+  
+  function myFunction() {
+    let x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+
+
   return (
-    <header>
-      <h1><Link to='/'>e-commerce</Link></h1>
-      <nav>
-        <ul>
-          <li><Link to='/login'>Login</Link></li>
-          <li><Link to='/register'>register</Link></li>
-          <li><Link to='/'>Purchases</Link></li>
-          <li><Link to='/'>Cart</Link></li>
-        </ul>
-      </nav>
-    </header>
-  )
-}
-export default Header
+    <div className="topnav" id="myTopnav">
+      <Link to="/">E-commerce</Link>
+      <Link to="/login">Login</Link>
+      <Link to="/register">register</Link>
+      <Link to="/">Purchases</Link>
+      <Link to="/">Cart</Link>
+      <a  className="icon" onClick={myFunction}>
+        <i className="bx bx-menu"></i>
+      </a>
+    </div>
+  );
+};
+export default Header;

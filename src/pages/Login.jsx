065c1789bb1f6506d882bdc616232f9b2ae43572
ANGELGOMEZ2007 <aagc8969@gmail.com>
2023-05-08 16:39:28 -0400

    login y register

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import useAuthentication from '../hooks/useAuthentication'
-
+import "./styles/Login.css"
+import { Link } from 'react-router-dom'
 const Login = () => {
 
     const { loginUser } = useAuthentication()
@@ -13,17 +14,23 @@ const Login = () => {
     }
 
     return (
-        <form onSubmit={handleLogin}>
-            <div>
-                <label htmlFor="email">Email</label>
-                <input type="email" id='email' />
+        <div className="form__user">
+            <div className="form__user-container">
+                <h2>Welcome! Enter your email and password to continue</h2>
+                <form onSubmit={handleLogin}>
+                    <div className='input__container'>
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id='email' />
+                    </div>
+                    <div className='input__container'>
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id='password' />
+                    </div>
+                    <button>Login</button>
+                </form>
+                <p>Don't have an account? <span><a href=""><Link to="/register">Sign up</Link></a></span></p>
             </div>
-            <div>
-                <label htmlFor="password">Password</label>
-                <input type="password" id='password' />
-            </div>
-            <button>Sign in</button>
-        </form>
+        </div>
     )
 }
 
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -3,6 +3,7 @@ import useFetch from '../hooks/useFetch'
 import { useForm } from 'react-hook-form'
 import useAuthentication from '../hooks/useAuthentication'
 import defaultRegisterValues from '../components/utils/defaultRegisterValues'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
 
@@ -16,32 +17,37 @@ const Register = () => {
 
     }
 
-  return (
-   <form onSubmit={handleSubmit(sumit)}>
-    <h1>Create a new User</h1>
-    <div>
-        <label htmlFor="firstName">First Name</label>
-        <input {...register('firstName')} type="text" id='firstName' />
-    </div>
-    <div>
-        <label htmlFor="lasttName">Last Name</label>
-        <input {...register('lastName')} type="text" id='lasttName' />
-    </div>
-    <div>
-        <label htmlFor="email">Email</label>
-        <input {...register('email')} type="text" id='email' />
-    </div>
-    <div>
-        <label htmlFor="password">Password</label>
-        <input {...register('password')}  type="text" id='password' />
-    </div>
-    <div>
-        <label htmlFor="phone">Phone</label>
-        <input {...register('phone')}  type="tel" id='phone' />
-    </div>
-    <button>Register</button>
-   </form>
-  )
+    return (
+        <div className="form__user">
+            <div className="form__user-container">
+                <form onSubmit={handleSubmit(sumit)}>
+                    <h2>Create a new User</h2>
+                    <div className='input__container'>
+                        <label htmlFor="firstName">First Name</label>
+                        <input {...register('firstName')} type="text" id='firstName' />
+                    </div>
+                    <div className='input__container'>
+                        <label htmlFor="lasttName">Last Name</label>
+                        <input {...register('lastName')} type="text" id='lasttName' />
+                    </div>
+                    <div className='input__container'>
+                        <label htmlFor="email">Email</label>
+                        <input {...register('email')} type="text" id='email' />
+                    </div>
+                    <div className='input__container'>
+                        <label htmlFor="password">Password</label>
+                        <input {...register('password')} type="text" id='password' />
+                    </div>
+                    <div className='input__container'>
+                        <label htmlFor="phone">Phone</label>
+                        <input {...register('phone')} type="tel" id='phone' />
+                    </div>
+                    <button>Register</button>
+                </form>
+                <p>Already have an account?<span><a href=""><Link to="/login">Log in</Link></a></span></p>
+            </div>
+        </div>
+    )
 }
 
 export default Register
